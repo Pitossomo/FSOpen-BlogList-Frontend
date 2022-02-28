5f5147c06c7a062e5dc2eeda1ea2baebe6966082
pitossomo <pitossomo@gmail.com>
2022-02-28 17:30:55 -0300

    Toggable details in Blog component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleDetails = () => setShowDetails(!showDetails)
+
+  const detailsStyle = { display: showDetails ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} 
+        <button onClick={toggleDetails}>view</button>
+        <div style={detailsStyle} >
+          <p>{blog.url}</p>
+          <p>{blog.likes}</p>
+          <p>{blog.user.name}</p>
+        </div>
+      </div>
+    </div>
+  )  
+}
 
 export default Blog
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,13 +1,15 @@
 import React from "react"
 import Blog from "./Blog"
 
-const Blogs = ({blogs}) => (
-  <div>
-    <h2>blogs</h2>
-    { blogs.map(blog =>
-      <Blog key={blog.id} blog={blog} />
-    )}
-  </div>
-)
+const Blogs = ({blogs}) => {
+  return (
+    <div>
+      <h2>blogs</h2>
+      { blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
 
 export default Blogs
